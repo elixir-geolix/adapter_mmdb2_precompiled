980f0dc9682077bc2fdce66f7b24f7d0bbe9745d
Marc Neudert <marc.neudert@gmail.com>
2021-01-01 13:16:07 +0100

    Remove outdated verification configuration

--- verify/geolix/config/config.exs
+++ verify/geolix/config/config.exs
@@ -5,5 +5,4 @@ alias Geolix.Adapter.MMDB2Precompiled
 config :geolix,
   databases: [
     %{id: :country, adapter: MMDB2Precompiled, database: MMDB2Precompiled.VerifyCountry}
-  ],
-  pool: [size: 1, max_overflow: 0]
+  ]
